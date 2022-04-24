01ecda705dc50629da1992319b81965dbfd3f660
Anil Gubbala <anil.gubbala@sjsu.edu>
2022-04-23 19:34:52 -0700

    home page update

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 import { REDUCER } from "../utils/consts";
 import { redirectHome } from "../utils/redirector";
-import { Button, FloatingLabel, Form } from "react-bootstrap";
+import { Button, FloatingLabel, Form, Table } from "react-bootstrap";
 import { get } from "../utils/serverCall";
+import { displayError } from "../utils/messages";
 
 function Home() {
   const role = localStorage.getItem(REDUCER.ROLE);
@@ -12,14 +13,65 @@ function Home() {
     destination: "-1",
   };
   const [rideDetails, setRideDetails] = useState(defaultValues);
-  const checkAvailability = () => {
-    get(`/checkAvailability`, { rideDetails })
-      .then((response) => {
-        //display list of vehicles from db
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+  const [availableCars, setAvailableCars] = useState([]);
+
+  const dummyCars = [
+    {
+      id: "1",
+      model: "1",
+      type: "Economy",
+      time: "2",
+      fare: "20",
+      capacity: 4,
+    },
+    {
+      id: "2",
+      model: "1",
+      type: "Premium",
+      time: "2",
+      fare: "20",
+      capacity: 6,
+    },
+    {
+      id: "3",
+      model: "1",
+      type: "Ecofriendly",
+      time: "2",
+      fare: "20",
+      capacity: 4,
+    },
+  ];
+
+  const validate = () => {
+    if (rideDetails.source === "-1") {
+      displayError("Select valid source");
+      return false;
+    }
+    if (rideDetails.destination === "-1") {
+      displayError("Select valid detination");
+      return false;
+    }
+    if (rideDetails.source === rideDetails.destination) {
+      displayError("Select different source and destinations");
+      return false;
+    }
+    return true;
+  };
+
+  const checkAvailability = (e) => {
+    e.preventDefault();
+    if (!validate()) {
+      return;
+    }
+    setAvailableCars(dummyCars);
+    // get(`/checkAvailability`, { rideDetails })
+    //   .then((response) => {
+    //     //display list of vehicles from db
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   };
 
   if (role !== "0") {
@@ -27,51 +79,94 @@ function Home() {
   }
   return (
     <div>
-      <Form style={{ maxWidth: "600px", margin: "auto" }}>
-        <div className="row">
-          <Form.Group className="col">
-            <FloatingLabel label="Source">
-              <Form.Control
-                as="select"
-                default="-1"
-                onChange={(e) => {
-                  setRideDetails({ ...rideDetails, source: e.target.value });
-                }}
-              >
-                <option value="-1">Select Source</option>
-                <option value="0">San Jose</option>
-                <option value="1">Santa Clara</option>
-              </Form.Control>
-            </FloatingLabel>
-          </Form.Group>
-        </div>
-        <div className="row">
-          <Form.Group className="col">
-            <FloatingLabel label="Destination">
-              <Form.Control
-                as="select"
-                onChange={(e) => {
-                  setRideDetails({
-                    ...rideDetails,
-                    destination: e.target.value,
-                  });
-                }}
-              >
-                <option value="-1">Select Destination</option>
-                <option value="0">San Jose</option>
-                <option value="1">Santa Clara</option>
-              </Form.Control>
-            </FloatingLabel>
-          </Form.Group>
-        </div>
-        <br />
+      <div className="row">
+        <Form style={{ maxWidth: "600px", margin: "auto" }}>
+          <div className="row">
+            <Form.Group className="col">
+              <FloatingLabel label="Source">
+                <Form.Control
+                  as="select"
+                  default="-1"
+                  onChange={(e) => {
+                    setRideDetails({ ...rideDetails, source: e.target.value });
+                  }}
+                >
+                  <option value="-1">Select Source</option>
+                  <option value="0">San Jose</option>
+                  <option value="1">Santa Clara</option>
+                </Form.Control>
+              </FloatingLabel>
+            </Form.Group>
+          </div>
+          <div className="row">
+            <Form.Group className="col">
+              <FloatingLabel label="Destination">
+                <Form.Control
+                  as="select"
+                  onChange={(e) => {
+                    setRideDetails({
+                      ...rideDetails,
+                      destination: e.target.value,
+                    });
+                  }}
+                >
+                  <option value="-1">Select Destination</option>
+                  <option value="0">San Jose</option>
+                  <option value="1">Santa Clara</option>
+                </Form.Control>
+              </FloatingLabel>
+            </Form.Group>
+          </div>
+          <br />
 
-        <div>
-          <Button type="submit" onClick={checkAvailability} variant="dark">
-            Check Availability
-          </Button>
-        </div>
-      </Form>
+          <div>
+            <Button
+              type="submit"
+              onClick={checkAvailability}
+              variant="dark"
+              style={{ marginBottom: "8px" }}
+            >
+              Check Availability
+            </Button>
+          </div>
+        </Form>
+      </div>
+      <div className="row">
+        <Table
+          striped
+          bordered
+          hover
+          size="sm"
+          style={{ margin: "auto", maxWidth: "800px" }}
+        >
+          <thead>
+            <tr>
+              <th>Model</th>
+              <th>Type</th>
+              <th>Capacity</th>
+              <th>Arrival Time</th>
+              <th>Fare</th>
+              <th>Book</th>
+            </tr>
+          </thead>
+          <tbody>
+            {availableCars.map((each) => {
+              return (
+                <tr key={each.id}>
+                  <td>{each.model}</td>
+                  <td>{each.type}</td>
+                  <td>{each.capacity}</td>
+                  <td>{each.time}</td>
+                  <td>{each.fare}</td>
+                  <td>
+                    <Button variant="dark">Book Ride</Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 }
