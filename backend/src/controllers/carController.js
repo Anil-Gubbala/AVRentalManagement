4ed076d1edabd725950724103badd2f99a5aeeea
saitejayeniganti <42708541+saitejayeniganti@users.noreply.github.com>
2022-04-23 18:03:26 -0700

    add car

--- backend/src/controllers/carController.js
+++ backend/src/controllers/carController.js
@@ -3,27 +3,32 @@ const jwt = require("jsonwebtoken");
 const { SECRET } = require("../../configuration");
 
 const addCar = (req, res) => {
-  console.log(req.body.carDetails);
-
   let { number, model, make, color, build, status, capacity } =
     req.body.carDetails;
 
-  let sql =
-    "INSERT into Cars (regNumber,model,make,color,build,status,capacity) values (?,?,?,?,?,?,?) ";
-  conn.query(
-    sql,
-    [number, model, make, color, build, status, capacity],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        throw err;
-      }
-      res.writeHead(200, {
-        "Content-Type": "text/plain",
-      });
-      res.send();
+  let sql = "Select * from Cars where Cars.ownerId=? and regNumber=?";
+  conn.query(sql, [req.user.email, number], (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
     }
-  );
+    console.log("car details present in DB - " + result);
+    if (result.length === 0)
+      sql =
+        "INSERT into Cars (ownerId,regNumber,model,make,color,build,status,capacity) values (?,?,?,?,?,?,?,?) ";
+    conn.query(
+      sql,
+      [req.user.email, number, model, make, color, build, status, capacity],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          response.error(res, 500, err.code);
+          return;
+        }
+        res.send(result);
+      }
+    );
+  });
 };
 
 module.exports = { addCar };
